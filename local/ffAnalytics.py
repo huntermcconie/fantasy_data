Fixed column order of tables to be consistent. Added Wins, Losses, WinPct, Average points.

local/ffAnalytics.py
@@ -10,35 +10,44 @@ def ffDfMatch (yearIn, weekListIn, jsObj):
             game['home']['totalPoints'],   
             yearIn,
             game['matchupPeriodId'],
-            np.where(game['winner']=="HOME",1,np.where(game['winner']=="AWAY",0,'UNDECIDED')),
+            np.where(game['matchupPeriodId']<14,'Regular','Playoff'),
+            np.where(game['winner']=="HOME",1,np.where(game['winner']=="AWAY",0,2)).astype(int),
             1,
             game['home']['teamId'],    
-            np.where(game['matchupPeriodId']<14,0,1),
     ] for game in jsObj['schedule'] if game['matchupPeriodId'] in weekListIn]
-    ,columns=['Points','Season','Week','winFlg','homeFlg','teamId','playoffFlg'])
-
+    ,columns=['Points','Season','Week','Type','winFlg','homeFlg','teamId'])
+    
     dfAway = pd.DataFrame([[
             game['away']['totalPoints'], 
             yearIn,
             game['matchupPeriodId'],
-            np.where(game['winner']=="HOME",0,np.where(game['winner']=="AWAY",1,'UNDECIDED')),
+            np.where(game['matchupPeriodId']<14,'Regular','Playoff'),
+            np.where(game['winner']=="HOME",0,np.where(game['winner']=="AWAY",1,2)).astype(int),
             0,
             game['away']['teamId'],
-            np.where(game['matchupPeriodId']<14,0,1),
     ] for game in jsObj['schedule'] if game['matchupPeriodId'] in weekListIn]
-    ,columns=['Points','Season','Week','winFlg','homeFlg','teamId','playoffFlg'])
+    ,columns=['Points','Season','Week','Type','winFlg','homeFlg','teamId'])
     
     # merge home and away
     dfMatch = pd.concat([dfHome,dfAway])
 
     # delete match data that has not finished
-    dfMatch = dfMatch[dfMatch['winFlg'] != 'UNDECIDED']
-    
-    # convert to int
-    dfMatch['winFlg'] = dfMatch['winFlg'].astype(int)
+    dfMatch = dfMatch[dfMatch['winFlg'] != 2]
     
     # return df
     return dfMatch
+
+# function that takes url and returns ff team data df
+def ffDfteam (url):
+    # request and create team json object then create team df
+    teamJS = requests.get(url, params={"view": "mTeam"}).json()
+    
+    dfTeam = pd.DataFrame([[
+        team['id'],
+        str(team['location']) + " " + str(team['nickname'])
+    ] for team in teamJS['teams']], columns=['teamId','Name'])
+    
+    return dfTeam
 
 
 # function for returning combined ff match df based on web inputs
@@ -69,16 +78,8 @@ def ffApiPull (leagueId, yearBeg, yearEnd, weekBeg, weekEnd):
         # call matchDf function to create df and append to array        
         dfMatchGrp.append(ffDfMatch(year, weekList, matchJS))
 
-    # request and create team json object then create team df
-    teamJS = requests.get(urlCur, params={"view": "mTeam"}).json()
-    dfTeam = pd.DataFrame([[
-        team['id'],
-        str(team['location']) + " " + str(team['nickname'])
-    ] for team in teamJS['teams']], columns=['teamId','Name'])
-    
-    #create total matches df and merge
-    dfMatches = pd.concat(dfMatchGrp)
-    dfOut = pd.merge(dfMatches, dfTeam, how='left',on='teamId').drop(columns=['teamId'])[['Name','Points','Season','Week','winFlg','homeFlg','playoffFlg']]
+    #create total matches df after merging match data and joining with team data by calling function 
+    dfOut = pd.merge(pd.concat(dfMatchGrp), ffDfteam(urlCur), how='left',on='teamId').drop(columns=['teamId'])[['Name','Points','Season','Week','winFlg','homeFlg','Type']]
     
     #return mrg df
     return dfOut.sort_values(by=['Season','Name','Week'])
@@ -86,19 +87,35 @@ def ffApiPull (leagueId, yearBeg, yearEnd, weekBeg, weekEnd):
 
 # function for totalPoints by team
 def ffTotalPoints (df):
-    return df.groupby(['Name']).agg({'Points':'sum','winFlg':'sum'}).sort_values(by=['Points'], ascending=False).rename(columns={"winFlg": "Wins"})
+    df = df.groupby(['Name'],as_index=False).agg({'Points':'sum','winFlg':['sum','count']}).reset_index(drop=True)
+    
+    df.columns = df.columns.get_level_values(0)
+    df.columns = ['Name','Points','Wins','Losses']
+    
+    df['Losses'] = df['Losses'] - df['Wins']
+    df['Average'] = df['Points'] / (df['Wins'] + df['Losses'])
+    df['WinPct'] = df['Wins'] / (df['Wins'] + df['Losses'])    
+
+    return df.sort_values(by=['Points'], ascending=False)
+
+# function for printing totalPoints by team
+def ffTopSzns (df):
+    df = df.groupby(['Name','Season'],as_index=False).agg({'Points':'sum','winFlg':['sum','count']}).reset_index(drop=True)
+    
+    df.columns = ['Name','Season','Points','Wins','Losses']
+    
+    df['Losses'] = df['Losses'] - df['Wins']
+    df['Average'] = df['Points'] / (df['Wins'] + df['Losses'])
+    df['WinPct'] = df['Wins'] / (df['Wins'] + df['Losses'])    
+    
+    return df.nlargest(10,'Points').sort_values(by=['Points'], ascending=False)
+
 
 # function for top10 week scores
 def ffTopWeeks (df):
-    return df[['Points','Season','Week','playoffFlg']].nlargest(10,'Points')
+    return df[['Name','Points','Season','Week','Type']].nlargest(10,'Points')
     
     
 # function for bot10 week scores
 def ffBotWeeks (df):
-    return df[['Points','Season','Week','playoffFlg']].nsmallest(10,'Points')
-
-
-# function for printing totalPoints by team
-def ffTopSzns (df):
-    df = df.groupby(['Name','Season'])['Points'].sum().reset_index().sort_values(by=['Points'], ascending=False)
-    return df[['Name','Points','Season']].nlargest(10,'Points')
+    return df[['Name','Points','Season','Week','Type']].nsmallest(10,'Points')